1e558e5e8727f9508636a198f0f8f3047a35199d
wintony <winton2095@gmail.com>
2026-05-19 16:24:30 -0400

    Read CLI argument for borough

--- nyc_complaints.py
+++ nyc_complaints.py
@@ -1,5 +1,15 @@
-def main():
-    print("Hello world")
+import argparse
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--borough", required=True)
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    borough = args.borough
+
+    print(borough)
+    
 if __name__ == "__main__":
     main()
